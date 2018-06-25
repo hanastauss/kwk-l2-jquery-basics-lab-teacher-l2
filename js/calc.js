d8d0018e048c1d2502fd19a1f3b1d04ea9ad0cf1
Hana Stauss <hanastauss@icloud.com>
2018-06-25 16:38:32 +0000

    automatically backed up by learn

--- js/calc.js
+++ js/calc.js
@@ -1,2 +1,13 @@
 //add your code here
 
+$(document).ready(function(){
+  doMath(); 
+});
+
+var num1 = $('#number1');
+var num2 = $('#number2');
+var operator = $('#operation');
+
+function doMath(){
+  
+}
